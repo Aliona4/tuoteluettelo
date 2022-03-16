f248261593056f25fd575d9a06d820f821f81278
new user <s2101121@edu.bc.fi>
2022-03-16 16:05:10 +0200

    Added phone-details ts

--- src/app/phone-detail/phone-detail.component.ts
+++ src/app/phone-detail/phone-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { PhoneService } from '../phone.service';
+
 
 @Component({
   selector: 'app-phone-detail',
@@ -7,15 +10,34 @@ import { PhoneService } from '../phone.service';
   styleUrls: ['./phone-detail.component.css']
 })
 export class PhoneDetailComponent implements OnInit {
-  
+  phoneId = '';
+  productList = [];
+  chosenPhone: any = '';
+  allReady = false;
 
   constructor(
-    private phoneService: PhoneService
+    private route: ActivatedRoute,
+    private phonoService: PhoneService,
+    private location: Location
     
   ) { }
   
   
   ngOnInit(): void {
+    this.getPhone();
   }
-
+  getPhone(): void {
+    this.phoneId = this.route.snapshot.params['id'];
+    this.route.params.subscribe((params) => {
+      this.phoneId = params['id'];
+    });
+    // this.phonoService.getProduct(this.phoneId).subscribe(((data: any) => {
+    //   this.chosenPhone = data;
+    //   this.allReady = true;
+    // }));
+  }
 }
+
+// goBack(): void {
+//   this:location.back(),
+// }
